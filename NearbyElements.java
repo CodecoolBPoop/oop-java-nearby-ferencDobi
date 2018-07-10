a8f97abcdfc19b5e8dd90a84f37d1ef15b2343e2
ferencDobi <ferencdbi@gmail.com>
2018-07-10 19:12:20 +0200

    algo implement

---
+++ NearbyElements.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class NearbyElements {
+    private static int[][] multi = new int[][]{
+        { 2, 0, 4, 1241, 12, 5, 11, 1110, -42, 424, 1, 12323 },
+        { 1, 3, 5, 7 },
+        { 321, 320, 32, 3, 41241, -11, -12, -13, -66, -688 }
+    };
+
+    public static int[] nearby(int x, int y, int range) {
+        int lastIndex = multi[x].length - 1;
+        int itemsBefore = (y < range) ? y : range;
+        int itemsAfter = (lastIndex - y < range) ? lastIndex - y : range; 
+        int[] items = new int[itemsBefore + itemsAfter];
+
+        for (int i = y - itemsBefore; i < y + itemsAfter; i++) {
+            items[i] = (i >= y) ? multi[x][i+1] : multi[x][i];
+        }
+
+        return items;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(nearby(0, 2, 2))); // [2, 0, 1241, 12]
+        System.out.println(Arrays.toString(nearby(1, 0, 1))); // [3]
+        System.out.println(Arrays.toString(nearby(1, 3, 5))); // [1, 3, 5]
+    }
+}
